46a81ca35fd2fa8824ff77fceabd2970411301e0
Ryan <121324625+ryanlkl@users.noreply.github.com>
2023-07-26 00:23:48 +0100

    Turtle Graphics

---
+++ Day 18/draw_square.py
@@ -0,0 +1,62 @@
+from turtle import *
+
+timmy = Turtle()
+
+timmy.shape("circle")
+timmy.color("blue")
+
+# Drawing a dashed line
+#for moves in range(0,50):
+#    timmy.forward(3)
+#    timmy.color("white")
+#    timmy.forward(3)
+#    timmy.color("blue")
+
+# Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon
+# and decagon
+# Do so by calculating inner angle and using number of sides
+
+
+#outside_angle = 180 - inside_angle
+#for moves in range(0,3):
+#    timmy.forward(100)
+#    timmy.left(120)
+
+# Square
+#for moves in range(0,4):
+#  timmy.forward(100)
+#  timmy.left(90)
+
+# Pentagon
+#for moves in range(0,5):
+#    timmy.forward(100)
+#    timmy.left(72)
+
+# Hexagon
+#for moves in range(0,6):
+#    timmy.forward(100)
+#    timmy.left(60)
+
+# Heptagon
+#for moves in range(0,7):#
+#    timmy.forward(100)
+#    timmy.left(180 - 900/7)
+
+# Octagon
+#for moves in range(0,8):
+#    timmy.forward(100)
+#    timmy.left(180 - 1080/8)
+
+# Nonagon
+#for moves in range(0,9):
+#    timmy.fd(100)
+#    timmy.left(180 - 1260/9)
+
+# Decagon
+#for moves in range(0,10):
+#    timmy.fd(100)
+#    timmy.left(36)
+
+
+screen = Screen()
+screen.exitonclick()
