bda063c4ddb87ebdd6aead44f6a70dc2621fdeb8
Ryan <121324625+ryanlkl@users.noreply.github.com>
2023-09-09 18:43:52 +0100

    Day 66 Completed! I created my own RESTful API using python and postman

---
+++ Advanced/Day 66/main.py
@@ -0,0 +1,155 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+from decouple import config
+
+'''
+Install the required packages first:
+Open the Terminal in PyCharm (bottom left).
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+API_KEY = config("API_KEY")
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "map_url": self.map_url,
+            "img_url": self.img_url,
+            "location": self.location,
+            "seats": self.seats,
+            "has_toilet": self.has_toilet,
+            "has_wifi": self.has_wifi,
+            "has_sockets": self.has_sockets,
+            "can_take_calls": self.can_take_calls,
+            "coffee_price": self.coffee_price
+        }
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+## HTTP GET - Read Record
+@app.route("/random")
+def get_random_cafe():
+    cafe = db.session.execute(db.select(Cafe))
+    all_cafes = cafe.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price
+    })
+
+@app.route("/all")
+def get_all():
+    all_cafes = db.session.execute(db.select(Cafe).order_by(Cafe.name)).scalars().all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+@app.route("/search")
+def search_for_cafe():
+    loc = request.args.get('loc')
+    condition = Cafe.location.like(f"%{loc}%")
+    found_cafes = db.session.query(Cafe).where(condition).order_by(Cafe.name)
+    cafes = [cafe.to_dict() for cafe in found_cafes]
+    return jsonify(cafes=cafes) if cafes else jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.get_or_404(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"Success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
+@app.route("/report-closed/<int:cafe_id>",methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == API_KEY:
+        cafe = db.session.get(Cafe,cafe_id)
+        if cafe:
+            name = cafe.name
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"Success": f"Successfully removed {name} from the database."})
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api-key"})
+
+
+
+
+## HTTP POST - Create Record
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
