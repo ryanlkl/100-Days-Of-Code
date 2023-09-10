512f5f90829d5890508d02aa32723f96412443c7
Ryan <121324625+ryanlkl@users.noreply.github.com>
2023-09-10 21:45:06 +0100

    Day 69 Completed! I created a fully functioning blog that allows only the admin to delete and edit posts while other users can post with their names and blogs

--- Advanced/Day 68/main.py
+++ Advanced/Day 68/main.py
@@ -32,18 +32,19 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # Passing True or False if the user is authenticated.
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         email = request.form.get('email')
-        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
         if user:
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for('login'))
-
         hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method='pbkdf2:sha256',
@@ -58,7 +59,8 @@ def register():
         db.session.commit()
         login_user(new_user)
         return redirect(url_for("secrets"))
-    return render_template("register.html",logged_in=current_user.is_authenticated)
+    # Passing True or False if the user is authenticated.
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -66,8 +68,8 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-
-        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
         if not user:
             flash("That email does not exist, please try again.")
             return redirect(url_for('login'))
@@ -77,14 +79,15 @@ def login():
         else:
             login_user(user)
             return redirect(url_for('secrets'))
+    # Passing True or False if the user is authenticated.
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
-    return render_template("login.html",logged_in=current_user.is_authenticated())
 
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html",name=current_user.name,logged_in=True)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
